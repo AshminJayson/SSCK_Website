8094ab174d07c2287a0c37700385505c17781dee
Ashmin Jayson <ashminjayson10@gmail.com>
2023-02-02 01:20:29 +0530

    Responsive Navbar and component alterations complete

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,28 @@
 "use strict"
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.nav-toggle')
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+    
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    }
+    else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+})
+
+
+const navLinks = document.querySelectorAll('.nav-link')
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () =>  {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    })
+})
+
